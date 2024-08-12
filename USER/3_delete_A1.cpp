90435dbf866b78ac5d4409aa0a89a6bbed498687
Carlos Guzmán <guzmanc@utb.edu.co>
2024-08-11 20:14:54 -0500

    chore: Update file A1.txt to reflect correct filename

--- USER/3_delete_A1.cpp
+++ USER/3_delete_A1.cpp
@@ -3,7 +3,7 @@
 #include <cstdio>
 //esta funcion elimina el archivo a1 sin importar donde este el ejecutable y en caso tal no pueda borrarlo que arroje un mensaje de error
 void borrarA1() {
-    const char* ruta_A1 = "C:\\A1.txt";  // Ruta del archivo A1 (no se puede manipular)
+    const char* ruta_A1 = "A1.txt";  // Ruta del archivo A1 (no se puede manipular)
 
     if (std::remove(ruta_A1) == 0) {
         std::cout << "El archivo A1 fue borrado correctamente." << std::endl;
